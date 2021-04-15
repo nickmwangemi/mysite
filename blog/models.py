bc6833a1d55152b2f7c6c2813e4146976d877ccb
nickmwangemi <nickmwangemi@gmail.com>
2021-04-15 20:15:43 +0300

    Added tagging functionality

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -36,6 +37,7 @@ class Post(models.Model):
         return reverse('blog:post_detail', 
         args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
+    tags = TaggableManager()
 
 class Comment(models.Model):
     post = models.ForeignKey(
